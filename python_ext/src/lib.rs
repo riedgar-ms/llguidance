db52cbd955aaeae7881f7d8b22e2f50707194369
Michal Moskal <michal@moskal.me>
2025-03-15 19:34:27 -0700

    add python json serializer

--- python_ext/src/lib.rs
+++ python_ext/src/lib.rs
@@ -3,6 +3,7 @@ use pyo3::prelude::*;
 mod llinterpreter;
 mod llmatcher;
 mod py;
+mod pyjson;
 
 // name must match the `lib.name` setting in the `Cargo.toml`
 #[pymodule]
---
+++ python_ext/src/pyjson.rs
@@ -0,0 +1,103 @@
+// based on https://github.com/mre/hyperjson/blob/master/src/lib.rs
+
+use pyo3::{
+    exceptions::PyValueError,
+    prelude::*,
+    types::{PyDict, PyFloat, PyList, PyTuple},
+};
+use serde::{
+    ser::{SerializeMap as _, SerializeSeq as _},
+    Serialize, Serializer,
+};
+
+pub struct SerializePyObject<'a> {
+    v: Bound<'a, PyAny>,
+}
+
+impl<'a> SerializePyObject<'a> {
+    pub fn new(v: Bound<'a, PyAny>) -> Self {
+        SerializePyObject { v }
+    }
+}
+
+pub fn to_json<'a>(v: Bound<'a, PyAny>) -> PyResult<serde_json::Value> {
+    let obj = SerializePyObject::new(v);
+    serde_json::to_value(&obj).map_err(|e| PyValueError::new_err(format!("{}", e)))
+}
+
+impl<'a> Serialize for SerializePyObject<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        macro_rules! extract {
+            ($t:ty) => {
+                if let Ok(val) = self.v.extract::<$t>() {
+                    return val.serialize(serializer);
+                }
+            };
+        }
+
+        fn debug_py_err<E: serde::ser::Error>(err: PyErr) -> E {
+            E::custom(format_args!("{:?}", err))
+        }
+
+        extract!(String);
+        extract!(bool);
+
+        if let Ok(x) = self.v.downcast::<PyFloat>() {
+            return x.value().serialize(serializer);
+        }
+
+        extract!(u64);
+        extract!(i64);
+
+        if self.v.is_none() {
+            return serializer.serialize_unit();
+        }
+
+        if let Ok(x) = self.v.downcast::<PyDict>() {
+            let mut map = serializer.serialize_map(Some(x.len()))?;
+            for (key, value) in x {
+                if let Ok(key) = key.str() {
+                    let key = key.to_string();
+                    map.serialize_key(&key)?;
+                } else {
+                    return Err(serde::ser::Error::custom(format_args!(
+                        "Dictionary key is not a string: {:?}",
+                        key
+                    )));
+                }
+                map.serialize_value(&SerializePyObject { v: value })?;
+            }
+            return map.end();
+        }
+
+        if let Ok(x) = self.v.downcast::<PyList>() {
+            let mut seq = serializer.serialize_seq(Some(x.len()))?;
+            for element in x {
+                seq.serialize_element(&SerializePyObject { v: element })?
+            }
+            return seq.end();
+        }
+
+        if let Ok(x) = self.v.downcast::<PyTuple>() {
+            let mut seq = serializer.serialize_seq(Some(x.len()))?;
+            for element in x {
+                seq.serialize_element(&SerializePyObject { v: element })?
+            }
+            return seq.end();
+        }
+
+        match self.v.repr() {
+            Ok(repr) => Err(serde::ser::Error::custom(format_args!(
+                "Value is not JSON serializable: {}",
+                repr,
+            ))),
+            Err(_) => Err(serde::ser::Error::custom(format_args!(
+                "Type is not JSON serializable: {}",
+                self.v.get_type().name().map_err(debug_py_err)?
+            ))),
+        }
+    }
+}
