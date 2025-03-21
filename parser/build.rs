c35cda3fb67d4452aa86e9e3c715b67fb62590ed
Michal Moskal <michal@moskal.me>
2025-03-21 18:48:49 +0000

    allow for building outside of cargo

--- parser/build.rs
+++ parser/build.rs
@@ -3,7 +3,26 @@ use std::env;
 fn main() {
     println!("cargo:rerun-if-changed=llguidance.h");
 
-    let copy_path = format!("{}/../../../llguidance.h", env::var("OUT_DIR").unwrap());
+    let required_vars = [
+        "CARGO",
+        "CARGO_MANIFEST_PATH",
+        "CARGO_PKG_NAME",
+        "CARGO_PKG_VERSION",
+        "RUST_VERSION",
+        "OUT_DIR",
+    ];
 
-    std::fs::copy("llguidance.h", copy_path).unwrap();
+    let missing: Vec<_> = required_vars
+        .iter()
+        .filter(|&&var| env::var(var).is_err())
+        .collect();
+
+    if !missing.is_empty() {
+        // this means we're running outside of cargo
+        println!("cargo:warning=Missing variables: {:?}", missing);
+    } else {
+        // otherwise, assume OUT_DIR is target/debug/build/llguidance-<hash>/out
+        let copy_path = format!("{}/../../../llguidance.h", env::var("OUT_DIR").unwrap());
+        std::fs::copy("llguidance.h", copy_path).unwrap();
+    }
 }
