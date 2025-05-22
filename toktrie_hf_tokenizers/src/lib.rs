6cae393b9c04fe67621615ff22b46beab512d069
Michal Moskal <michal@moskal.me>
2025-05-21 18:07:14 -0700

    improve special token detection

--- toktrie_hf_tokenizers/src/lib.rs
+++ toktrie_hf_tokenizers/src/lib.rs
@@ -114,7 +114,8 @@ impl ByteTokenizer {
         let mut specials = HashSet::new();
 
         for (id, info) in added.iter() {
-            if info.special {
+            // we treat all added tokens of the form <...> as special tokens
+            if info.special || (info.content.starts_with("<") && info.content.ends_with(">")) {
                 match info.content.as_str() {
                     "</s>"
                     | "<|endoftext|>"
